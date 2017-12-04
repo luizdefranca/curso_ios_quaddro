70c755f977d460006a2b3fb7862257937ca461bb
Luiz Ramos <luizramospe@hotmail.com>
2017-12-04 19:29:38 -0300

    tipos de variáveis em swift

--- principais_tipos_no_swift.playground/Contents.swift
+++ principais_tipos_no_swift.playground/Contents.swift
@@ -6,4 +6,21 @@
 //Bool(Boleano)
 //Armazena um valor true(verdadeiro) ou false(falso)
 
+var meuBool : Bool = true
+var meuBoolImplicito = false
+
+//Int (inteiros - numero sem casas decimais)
+//armazena valores inteiros positivos e negativos
+//pode armazena de 8 a 64bit conforme o tipo definido seja
+// Int, Int16, Int32 ou Int64
+
+//Signed - um inteiro que permite sinal (valores negativos)
+//Int8 == 8 bits = 2ˆ8 == 256
+
+var saldoBancario : Int8 =  -20
+
+//Unsigned - um inteiro que não permite valores negativos
+
+var saldoBancarioSemprePositivo : UInt8 = 20 //neste caso não pode ser atribuído valor negativo
+
 
