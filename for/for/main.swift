7ef80cc7535c135dff9413d79c575bbf7e5c9040
Luiz Ramos <luizramospe@hotmail.com>
2017-12-06 20:31:58 -0300

    switch

---
+++ for/for/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  for
+//
+//  Created by LuizRamos on 06/12/17.
+//  Copyright © 2017 LuizRamos. All rights reserved.
+//
+
+import Foundation
+
+
--- switch.playground/Contents.swift
+++ switch.playground/Contents.swift
@@ -1,5 +1,87 @@
-//: Playground - noun: a place where people can play
+/*
+ 
+ Switch - o switch é uma estrutura de decisão  que analisa caso a caso, é uma ótima opção quando se tem
+ diversos estados potenciais para uma verificação
+ 
+ sintaxe:
+ 
+ switch valorASerConsiderado{
+ 
+ case valor1:
+    executa caso seja igual ao valor1
+ case valor2:
+    executa caso seja igual ao valor2
+ case valor3:
+    executa caso seja igual ao valor3
+ case valor4:
+    executa caso seja igual ao valor4
+ default:
+    caso não seja nenhum dos valores anteriores
+ }
+ */
 
-import UIKit
+var consideracao = 10
 
-var str = "Hello, playground"
+switch consideracao {
+case 0:
+    print("Consideracao é 0")
+case 5:
+    print("Consideracao é 5")
+case 10:
+    print("Consideracao é 10")
+default:
+    print("Consideracao é desconhecido")
+}
+
+var letra = "a"
+switch letra {
+case  "a":
+    print("A letra \(letra) é vogal")
+case "e":
+    print("A letra \(letra) é vogal")
+case "i":
+    print("A letra \(letra) é vogal")
+case "o":
+    print("A letra \(letra) é vogal")
+case "u":
+    print("A letra \(letra) é vogal")
+default:
+    print("A letra \(letra) não é vogal")
+}
+
+//Consideração de múltiplos argumentos
+
+switch letra {
+case "a", "e", "i", "o", "u":
+    print("A letra \(letra) é vogal")
+default:
+    print("A letra \(letra) não é vogal")
+}
+
+//Fallthrough
+//A keyword fallthrough permite que o fluxo do swift continue sendo executado após primeira condição ser chamada
+
+
+var nivelHierarquico = 4
+
+switch nivelHierarquico {
+case 5:
+    print("Abrir o cofre")
+    fallthrough
+case 4:
+    print("Usar heliporto")
+     fallthrough
+case 3:
+    print("Usar transporte coorporativo")
+
+case 2:
+    print("Usar máquina de café")
+     fallthrough
+case 1:
+    print("Bater o ponto")
+     fallthrough
+
+
+default:
+    print("ter uma foto no crachá")
+}
