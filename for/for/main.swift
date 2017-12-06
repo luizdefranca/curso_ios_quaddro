9acf4a2199b27934dab29a443a3da2b79a527890
Luiz Ramos <luizramospe@hotmail.com>
2017-12-06 20:43:37 -0300

    for

--- for/for/main.swift
+++ for/for/main.swift
@@ -8,4 +8,37 @@
 
 import Foundation
 
+/*
+ 
+ *** Forma antiga do for ***
+ for(variavel inicial; condicao de parada; incremento){
+ bloco a ser executado
+ }
+ 
+ *** Forma atual do for ***
+ 
+ for controle in 0...5{
+ bloco a ser executado
+ }
+ */
 
+//For in
+//For com intervalo fechado, ou seja, utiliza do intervalor entre o primeiro e o último numero e inclui o último número
+
+for controle in 0...5{
+    print("loop numero \(controle)")
+}
+
+//For com intervalo semi-fechado, utiliza o intervalo entre o primeiro e o último número, porém não inclui o último número
+
+for controle in 0..<5{
+    print("loop numero \(controle)")
+}
+
+//For in, dentro de um array
+
+var fruteira = ["🍑", "🍊", "🥑", "🍍", "🍇"]
+
+for fruta in fruteira{
+    print("Fruta do dia: \(fruta)")
+}
---
+++ while/while/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  while
+//
+//  Created by LuizRamos on 06/12/17.
+//  Copyright © 2017 LuizRamos. All rights reserved.
+//
+
+import Foundation
+
