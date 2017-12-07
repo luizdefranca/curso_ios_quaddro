8244662ca69426c25278c9d2be2b7f087a547862
Luiz Ramos <luizramospe@hotmail.com>
2017-12-06 21:00:47 -0300

    while

--- while/while/main.swift
+++ while/while/main.swift
@@ -8,3 +8,35 @@
 
 import Foundation
 
+/*
+ while
+ 
+ sintaxe
+ 
+ while (condição){
+    bloco executado
+ }
+ 
+ 
+ */
+
+//condicao de repeticao controlada
+
+var contagem = 0
+while( contagem <= 5){
+    print("While numero \(contagem)")
+    contagem += 1
+}
+
+//condicao com quantidade loops não controlada
+var carros = ["Golf", "Uno", "BMW", "Ferrari Enzo", "Soul"]
+var variavelDeControle = 0
+var carroSelecionado = carros[variavelDeControle]
+
+
+while (carroSelecionado != "Ferrari Enzo"){
+    print(carroSelecionado," é o carro selecionado" )
+    
+    variavelDeControle += 1
+    carroSelecionado = carros[variavelDeControle]
+}
