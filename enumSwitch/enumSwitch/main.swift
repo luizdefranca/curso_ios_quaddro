5f6e1b0f315bc89afe46eba444c90f21118f262f
Luiz Ramos <luizramospe@hotmail.com>
2017-12-08 19:14:04 -0300

    Enums

---
+++ enumSwitch/enumSwitch/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  enumSwitch
+//
+//  Created by LuizRamos on 08/12/17.
+//  Copyright © 2017 LuizRamos. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
