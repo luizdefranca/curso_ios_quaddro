06db923e4ff9f2284ff8b59c373b549ad6544cc4
Luiz Ramos <luizramospe@hotmail.com>
2017-12-13 21:58:10 -0300

    UISwitch

--- UISwitch/UISwitch/ViewController.swift
+++ UISwitch/UISwitch/ViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var meuSwitch: UISwitch!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Propriedade que define o tintColor (off)
+        self.meuSwitch.tintColor = #colorLiteral(red: 1, green: 0.1491314173, blue: 0, alpha: 1)
+        
+        //propriedade que define a cor on
+        self.meuSwitch.onTintColor = #colorLiteral(red: 0.6254426927, green: 1, blue: 0.3592758311, alpha: 1)
+        
+        //propriedade que define a cor do Thumb
+        self.meuSwitch.thumbTintColor = #colorLiteral(red: 0.9994240403, green: 0.9855536819, blue: 0, alpha: 1)
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func dispararSwitch() {
+        
+        if self.meuSwitch.isOn{
+            self.view.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }else {
+            self.view.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        }
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.meuSwitch.setOn(!self.meuSwitch.isOn, animated: true)
+        self.dispararSwitch()
+        
+    }
+    
+    
 }
 
